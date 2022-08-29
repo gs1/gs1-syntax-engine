fae6285325e440d290432f6555a60faf9dfdb95b
Terry Burton <tez@terryburton.co.uk>
2022-08-29 01:30:26 +0100

    JS: Enhance Node.js example application

--- src/js-wasm/example.node.mjs
+++ src/js-wasm/example.node.mjs
@@ -2,6 +2,11 @@
  *  Basic node.js example for using the JavaScript wrapper around the WASM
  *  build of the GS1 Syntax Engine.
  *
+ *  Requirements:
+ *
+ *    - Node.js >= v13.2.0 for stable ES6 module support
+ *    - readline-sync package
+ *
  *  Copyright (c) 2022 GS1 AISBL.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,6 +30,8 @@
 
 "use strict";
 
+import readline from "readline-sync";
+
 
 /*
  * Temporary boilerplate required due to deficiencies in Emscripten's modulular
@@ -40,13 +47,107 @@ import { GS1encoder } from "./gs1encoder.mjs";
 var gs1encoder = new GS1encoder();
 await gs1encoder.init();
 
-var input = 'https://example.com/01/1231231231232/10/ABC123?99=TEST';
-console.log("Given GS1 DL URI:  " + input);
 
-gs1encoder.dataStr = input;
-console.log("AI element string: " + gs1encoder.aiDataStr);
+/*
+ *  If --version is provided, then report the library version and exit
+ *
+ */
+var argv = process.argv.slice(2);
+if (argv.length == 1 && argv == "--version") {
+    console.log("Library version: %s", gs1encoder.version);
+    gs1encoder.free();
+    process.exit(0);
+}
 
-console.log("HRI:");
-gs1encoder.hri.forEach(ai => console.log("       " + ai));
+
+/*
+ *  Interactive loop
+ *
+ */
+var exit = 0;
+while (!exit) {
+
+    console.log("\n\n\nCurrent state:");
+
+    console.log("\n    Barcode message:    %s", gs1encoder.dataStr);
+    console.log("\n    AI element string:  %s", gs1encoder.aiDataStr);
+
+    var dlURI = ''; try { dlURI = gs1encoder.getDLuri(null); } catch {}
+    console.log("\n    Digital Link URI:   %s", dlURI);
+
+    console.log("\n    HRI:\n");
+    gs1encoder.hri.forEach(ai => console.log("       %s", ai));
+
+    console.log("\n\nMENU:");
+    console.log("\n 1) Process raw barcode message data, either:");
+    console.log("      * Plain data");
+    console.log("      * Unbracketed AI element string with FNC1 in first position");
+    console.log("      * Digital Link URI");
+    console.log(" 2) Process a bracketed AI element string");
+    console.log(" 3) Process barcode scan data (prefixed by AIM Symbology Identifier)");
+
+    console.log("\n 4) Toggle 'include data titles in HRI' flag.  Current value = %s",
+                                        gs1encoder.includeDataTitlesInHRI ? "ON" : "OFF");
+    console.log(" 5) Toggle 'permit unknown AIs' flag.          Current value = %s",
+                                        gs1encoder.permitUnknownAIs ? "ON" : "OFF");
+    console.log(" 6) Toggle 'validate AI associations' flag.    Current value = %s",
+                                        gs1encoder.validateAIassociations ? "ON" : "OFF");
+
+    console.log("\n 0) Exit program");
+
+    var menuVal = await readline.question("\nMenu selection: ");
+
+    switch (menuVal) {
+        case "1":
+        case "2":
+        case "3":
+            var inpStr = await readline.question("\nEnter data: ");
+            if (!inpStr)
+                continue;
+            try {
+                if (menuVal === "1")
+                    gs1encoder.dataStr = inpStr;
+                else if (menuVal === "2")
+                    gs1encoder.aiDataStr = inpStr;
+                else            // "3"
+                    gs1encoder.scanData = inpStr;
+            } catch (err) {
+                console.log("\n\nERROR message: %s", err.message);
+                var markup = gs1encoder.errMarkup;
+                if (markup)
+                    console.log("ERROR markup:  %s", markup.replace(/\|/g, "⧚"));
+                continue;
+            }
+            break;
+        case "4":
+        case "5":
+        case "6":
+            var inpStr = await readline.question("\nEnter 0 for OFF or 1 for ON: ");
+            if (!inpStr)
+                continue;
+            if (inpStr !== "0" && inpStr !== "1") {
+                console.log("\n\nOUT OF RANGE. PLEASE ENTER 0 or 1");
+                continue;
+            }
+            try {
+                if (menuVal === "4")
+                    gs1encoder.includeDataTitlesInHRI = (inpStr === "1");
+                else if (menuVal === "5")
+                    gs1encoder.permitUnknownAIs = (inpStr === "1");
+                else           // "6"
+                    gs1encoder.validateAIassociations = (inpStr === "1");
+            } catch (err) {
+                console.log("\n\nERROR: %s", err.message);
+                continue;
+            }
+            break;
+        case "0":
+            exit = 1;
+            break;
+        default:
+            console.log("\n\nOUT OF RANGE. PLEASE ENTER 1-6, 9 or 0.");
+    }
+
+}
 
 gs1encoder.free();
