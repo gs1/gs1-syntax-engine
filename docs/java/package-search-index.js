2ea901a58c1954a9de2c678cf259db444a13b80c
Terry Burton <tez@terryburton.co.uk>
2025-11-05 22:15:45 +0000

    Refresh docs

---
+++ docs/java/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.gs1.gs1encoders"}];updateSearchResults();
--- docs/menudata.js
+++ docs/menudata.js
@@ -23,9 +23,5 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Data Structures",url:"annotated.html",children:[
-{text:"Data Structures",url:"annotated.html"},
-{text:"Data Structure Index",url:"classes.html"},
-{text:"Data Fields",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
-{text:"Variables",url:"functions_vars.html"}]}]}]}
+{text:"Overview",url:"index.html"},
+{text:"API Reference",url:"gs1encoders_8h.html"}]}
