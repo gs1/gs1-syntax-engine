d6219c18b69f7e55c745f86e0a23f320c0e4ee90
Terry Burton <tez@terryburton.co.uk>
2026-02-14 20:26:02 +0000

    Expand contrib Python wrapper

--- src/contrib/python3/example.py
+++ src/contrib/python3/example.py
@@ -21,28 +21,66 @@
 #  limitations under the License.
 #
 
-from gs1encoders import GS1Encoder
+from gs1encoders import GS1Encoder, Symbology, Validation
 
 
 def main():
-
-    data_str = "^010952123454321310ABC123^99TEST"
-    data_titles = True
 
     gs1encoder = GS1Encoder()
 
     print("\nVersion: {}\n".format(gs1encoder.get_version()))
 
-    gs1encoder.set_data_str(data_str)
+    # Set symbology to DataMatrix
+    gs1encoder.set_sym(Symbology.DM)
+    print("Symbology: {}".format(gs1encoder.get_sym().name))
 
-    print("IN: {}".format(gs1encoder.get_data_str()))
-    print("AI: {}".format(gs1encoder.get_ai_data_str() or "Not AI data"))
+    # Demonstrate validation control
+    print("RequisiteAIs validation enabled: {}".format(gs1encoder.get_validation_enabled(Validation.RequisiteAIs)))
+    gs1encoder.set_validation_enabled(Validation.RequisiteAIs, False)
+    print(
+        "RequisiteAIs validation enabled (after disable): {}".format(
+            gs1encoder.get_validation_enabled(Validation.RequisiteAIs)
+        )
+    )
 
-    gs1encoder.set_include_data_titles_in_hri(data_titles)
-    print("\nHRI{}:".format(" (including data titles)" if data_titles else ""))
+    # Set AI data
+    ai_data = "(01)09521234543213(10)ABC123(99)TEST"
+    gs1encoder.set_ai_data_str(ai_data)
+    print("\nAI IN:  {}".format(ai_data))
+    print("AI OUT: {}".format(gs1encoder.get_ai_data_str() or "Not AI data"))
+    print("DATA:   {}".format(gs1encoder.get_data_str()))
 
+    # HRI with data titles
+    gs1encoder.set_include_data_titles_in_hri(True)
+    print("\nHRI{}:".format(" (including data titles)" if gs1encoder.get_include_data_titles_in_hri() else ""))
     for h in gs1encoder.get_hri():
         print("    {}".format(h))
+
+    # DL URI
+    dl_uri = gs1encoder.get_dl_uri("https://example.com")
+    if dl_uri:
+        print("\nDL URI: {}".format(dl_uri))
+    else:
+        print("\nDL URI: Failed: {}".format(gs1encoder.get_err_markup()))
+
+    # Scan data round-trip
+    scan_data = gs1encoder.get_scan_data()
+    if scan_data:
+        print("Scan data: {}".format(scan_data))
+    else:
+        print("Scan data: None")
+
+    # DL ignored query params
+    ignored = gs1encoder.get_dl_ignored_query_params()
+    if ignored:
+        print("\nIgnored DL query params:")
+        for p in ignored:
+            print("    {}".format(p))
+
+    # Demonstrate boolean properties
+    print("\nAdd check digit: {}".format(gs1encoder.get_add_check_digit()))
+    print("Permit unknown AIs: {}".format(gs1encoder.get_permit_unknown_ais()))
+    print("Permit zero-suppressed GTIN in DL URIs: {}".format(gs1encoder.get_permit_zero_suppressed_gtin_in_dl_uris()))
 
 
 if __name__ == "__main__":
--- src/contrib/python3/gs1encoders.py
+++ src/contrib/python3/gs1encoders.py
@@ -22,6 +22,35 @@
 #
 
 import ctypes
+import enum
+import warnings
+
+
+class Symbology(enum.IntEnum):
+    NONE = -1
+    DataBarOmni = 0
+    DataBarTruncated = 1
+    DataBarStacked = 2
+    DataBarStackedOmni = 3
+    DataBarLimited = 4
+    DataBarExpanded = 5
+    UPCA = 6
+    UPCE = 7
+    EAN13 = 8
+    EAN8 = 9
+    GS1_128_CCA = 10
+    GS1_128_CCC = 11
+    QR = 12
+    DM = 13
+    DotCode = 14
+
+
+class Validation(enum.IntEnum):
+    MutexAIs = 0
+    RequisiteAIs = 1
+    RepeatedAIs = 2
+    DigSigSerialKey = 3
+    UnknownAInotDLattr = 4
 
 
 class GS1Encoder:
@@ -40,6 +69,61 @@ class GS1Encoder:
     __api.gs1_encoder_getErrMsg.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
     __api.gs1_encoder_getErrMsg.restype = ctypes.c_char_p
 
+    __api.gs1_encoder_getErrMarkup.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+    __api.gs1_encoder_getErrMarkup.restype = ctypes.c_char_p
+
+    __api.gs1_encoder_getSym.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+    __api.gs1_encoder_getSym.restype = ctypes.c_int
+
+    __api.gs1_encoder_setSym.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_int,
+    ]
+    __api.gs1_encoder_setSym.restype = ctypes.c_bool
+
+    __api.gs1_encoder_getAddCheckDigit.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+    __api.gs1_encoder_getAddCheckDigit.restype = ctypes.c_bool
+
+    __api.gs1_encoder_setAddCheckDigit.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_bool,
+    ]
+    __api.gs1_encoder_setAddCheckDigit.restype = ctypes.c_bool
+
+    __api.gs1_encoder_getPermitUnknownAIs.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+    __api.gs1_encoder_getPermitUnknownAIs.restype = ctypes.c_bool
+
+    __api.gs1_encoder_setPermitUnknownAIs.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_bool,
+    ]
+    __api.gs1_encoder_setPermitUnknownAIs.restype = ctypes.c_bool
+
+    __api.gs1_encoder_getPermitZeroSuppressedGTINinDLuris.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+    __api.gs1_encoder_getPermitZeroSuppressedGTINinDLuris.restype = ctypes.c_bool
+
+    __api.gs1_encoder_setPermitZeroSuppressedGTINinDLuris.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_bool,
+    ]
+    __api.gs1_encoder_setPermitZeroSuppressedGTINinDLuris.restype = ctypes.c_bool
+
+    __api.gs1_encoder_getValidationEnabled.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_int,
+    ]
+    __api.gs1_encoder_getValidationEnabled.restype = ctypes.c_bool
+
+    __api.gs1_encoder_setValidationEnabled.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_int,
+        ctypes.c_bool,
+    ]
+    __api.gs1_encoder_setValidationEnabled.restype = ctypes.c_bool
+
+    __api.gs1_encoder_getIncludeDataTitlesInHRI.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+    __api.gs1_encoder_getIncludeDataTitlesInHRI.restype = ctypes.c_bool
+
     __api.gs1_encoder_setIncludeDataTitlesInHRI.argtypes = [
         ctypes.POINTER(ctypes.c_void_p),
         ctypes.c_bool,
@@ -57,6 +141,33 @@ class GS1Encoder:
 
     __api.gs1_encoder_getAIdataStr.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
     __api.gs1_encoder_getAIdataStr.restype = ctypes.c_char_p
+
+    __api.gs1_encoder_setAIdataStr.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_char_p,
+    ]
+    __api.gs1_encoder_setAIdataStr.restype = ctypes.c_bool
+
+    __api.gs1_encoder_getScanData.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+    __api.gs1_encoder_getScanData.restype = ctypes.c_char_p
+
+    __api.gs1_encoder_setScanData.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_char_p,
+    ]
+    __api.gs1_encoder_setScanData.restype = ctypes.c_bool
+
+    __api.gs1_encoder_getDLuri.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.c_char_p,
+    ]
+    __api.gs1_encoder_getDLuri.restype = ctypes.c_char_p
+
+    __api.gs1_encoder_getDLignoredQueryParams.argtypes = [
+        ctypes.POINTER(ctypes.c_void_p),
+        ctypes.POINTER(ctypes.POINTER(ctypes.c_char_p)),
+    ]
+    __api.gs1_encoder_getDLignoredQueryParams.restype = ctypes.c_int
 
     __api.gs1_encoder_getHRI.argtypes = [
         ctypes.POINTER(ctypes.c_void_p),
@@ -83,13 +194,72 @@ class GS1Encoder:
     def get_version(self):
         return self.__api.gs1_encoder_getVersion().decode("utf-8")
 
+    def get_err_markup(self):
+        return self.__api.gs1_encoder_getErrMarkup(self.__ctx).decode("utf-8")
+
+    def get_sym(self):
+        return Symbology(self.__api.gs1_encoder_getSym(self.__ctx))
+
+    def set_sym(self, sym):
+        ret = self.__api.gs1_encoder_setSym(self.__ctx, int(sym))
+        if not ret:
+            raise GS1EncoderParameterException(self.get_err_msg())
+
+    def get_add_check_digit(self):
+        return bool(self.__api.gs1_encoder_getAddCheckDigit(self.__ctx))
+
+    def set_add_check_digit(self, value):
+        ret = self.__api.gs1_encoder_setAddCheckDigit(self.__ctx, 1 if value else 0)
+        if not ret:
+            raise GS1EncoderParameterException(self.get_err_msg())
+
+    def get_permit_unknown_ais(self):
+        return bool(self.__api.gs1_encoder_getPermitUnknownAIs(self.__ctx))
+
+    def set_permit_unknown_ais(self, value):
+        ret = self.__api.gs1_encoder_setPermitUnknownAIs(self.__ctx, 1 if value else 0)
+        if not ret:
+            raise GS1EncoderParameterException(self.get_err_msg())
+
+    def get_permit_zero_suppressed_gtin_in_dl_uris(self):
+        return bool(self.__api.gs1_encoder_getPermitZeroSuppressedGTINinDLuris(self.__ctx))
+
+    def set_permit_zero_suppressed_gtin_in_dl_uris(self, value):
+        ret = self.__api.gs1_encoder_setPermitZeroSuppressedGTINinDLuris(self.__ctx, 1 if value else 0)
+        if not ret:
+            raise GS1EncoderParameterException(self.get_err_msg())
+
+    def get_validation_enabled(self, validation):
+        return bool(self.__api.gs1_encoder_getValidationEnabled(self.__ctx, int(validation)))
+
+    def set_validation_enabled(self, validation, enabled):
+        ret = self.__api.gs1_encoder_setValidationEnabled(self.__ctx, int(validation), 1 if enabled else 0)
+        if not ret:
+            raise GS1EncoderParameterException(self.get_err_msg())
+
+    def get_validate_ai_associations(self):
+        warnings.warn(
+            "Use get_validation_enabled(Validation.RequisiteAIs) instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self.get_validation_enabled(Validation.RequisiteAIs)
+
+    def set_validate_ai_associations(self, value):
+        warnings.warn(
+            "Use set_validation_enabled(Validation.RequisiteAIs, value) instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self.set_validation_enabled(Validation.RequisiteAIs, value)
+
     def set_include_data_titles_in_hri(self, value):
         ret = self.__api.gs1_encoder_setIncludeDataTitlesInHRI(self.__ctx, 1 if value else 0)
         if not ret:
             raise GS1EncoderParameterException(self.get_err_msg())
 
     def get_include_data_titles_in_hri(self):
-        return self.__api.gs1_encoder_getIncludeDataTitlesInHRI(self.__ctx) == 1
+        return bool(self.__api.gs1_encoder_getIncludeDataTitlesInHRI(self.__ctx))
 
     def get_data_str(self):
         return self.__api.gs1_encoder_getDataStr(self.__ctx).decode("utf-8")
@@ -105,12 +275,43 @@ class GS1Encoder:
             return None
         return ret.decode("utf-8")
 
+    def set_ai_data_str(self, value):
+        ret = self.__api.gs1_encoder_setAIdataStr(self.__ctx, value.encode("utf-8"))
+        if not ret:
+            raise GS1EncoderParameterException(self.get_err_msg())
+
+    def get_scan_data(self):
+        ret = self.__api.gs1_encoder_getScanData(self.__ctx)
+        if not ret:
+            return None
+        return ret.decode("utf-8")
+
+    def set_scan_data(self, value):
+        ret = self.__api.gs1_encoder_setScanData(self.__ctx, value.encode("utf-8"))
+        if not ret:
+            raise GS1EncoderScanDataException(self.get_err_msg())
+
+    def get_dl_uri(self, stem=None):
+        c_stem = stem.encode("utf-8") if stem else None
+        ret = self.__api.gs1_encoder_getDLuri(self.__ctx, c_stem)
+        if not ret:
+            return None
+        return ret.decode("utf-8")
+
+    def get_dl_ignored_query_params(self):
+        ptr = ctypes.pointer(ctypes.c_char_p())
+        size = self.__api.gs1_encoder_getDLignoredQueryParams(self.__ctx, ctypes.byref(ptr))
+        params = []
+        for i in range(size):
+            params.append(ptr[i].decode("utf-8"))
+        return params
+
     def get_hri(self):
         ptr = ctypes.pointer(ctypes.c_char_p())
         size = self.__api.gs1_encoder_getHRI(self.__ctx, ctypes.byref(ptr))
-        hri = [None] * size
+        hri = []
         for i in range(size):
-            hri[i] = ptr[i].decode("utf-8")
+            hri.append(ptr[i].decode("utf-8"))
         return hri
 
 
